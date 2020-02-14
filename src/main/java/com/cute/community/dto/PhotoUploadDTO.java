15a8ff11cdc98c206995d495a40833536c20a990
baobao-sudo <2581901873@qq.com>
2020-02-14 19:32:05 +0800

    git init

---
+++ src/main/java/com/cute/community/dto/PhotoUploadDTO.java
@@ -0,0 +1,21 @@
+package com.cute.community.dto;
+
+import lombok.Data;
+
+@Data
+public class PhotoUploadDTO {
+    /**
+     * 文件原始名
+     */
+    private String fileName;
+    /**
+     * 后缀 类似.ppt
+     */
+    private String suffix;
+    /**
+     * 新名称 以19700101开始的毫秒
+     */
+    private String newFileName;
+
+    private String photoUrl;
+}
