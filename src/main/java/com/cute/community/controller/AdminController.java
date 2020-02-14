9be8a3c65a5aa4c0e4cc6d16baac43398c02ead9
baobao-sudo <2581901873@qq.com>
2020-02-14 19:25:56 +0800

    git init

--- src/main/java/com/cute/community/controller/AdminController.java
+++ src/main/java/com/cute/community/controller/AdminController.java
@@ -2,17 +2,19 @@ package com.cute.community.controller;
 
 
 import com.cute.community.accessctro.RoleContro;
-import com.cute.community.dto.UserDTO;
 import com.cute.community.enums.RoleEnum;
+import com.cute.community.form.UserRegisterForm;
+import com.cute.community.form.UserUpdateForm;
 import com.cute.community.service.UserService;
+import com.cute.community.vo.ResultVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 
 /**
@@ -23,7 +25,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/admin")
 @RestController
-@Api(tags = "注册接口")
+@Api(tags = "管理员接口")
 @CrossOrigin
 public class AdminController {
 
@@ -33,8 +35,30 @@ public class AdminController {
     @PostMapping("/addUser")
     @ApiOperation("添加用户")
     @RoleContro(role = RoleEnum.ADMIN)
-    public Object addUser(UserDTO userDTO) {
-        return userService.addUser(userDTO);
+    public Object addUser(@Valid UserRegisterForm registerForm, BindingResult bindingResult) {
+        return userService.addUser(registerForm,bindingResult);
     }
+
+    @GetMapping("/showAll")
+    @ApiOperation("展示所有用户")
+    @RoleContro(role = RoleEnum.ADMIN)
+    public ResultVO showAll() {
+        return userService.showAll();
+    }
+
+    @PostMapping("/deleteUser")
+    @ApiOperation("删除用户")
+    @RoleContro(role = RoleEnum.ADMIN)
+    public ResultVO deleteUser(@RequestParam("stuId") String stuId) {
+        return userService.deleteUser(stuId);
+    }
+
+    @PostMapping("/updateUser")
+    @ApiOperation("修改用户信息")
+    @RoleContro(role = RoleEnum.ADMIN)
+    public ResultVO updateUser(@Valid UserUpdateForm userUpdateForm, BindingResult bindingResult) {
+        return userService.updateUser(userUpdateForm,bindingResult);
+    }
+
 
 }
